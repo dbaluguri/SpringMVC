1a963ad082378e9013492886860ecd5fc8fb9cd4
Dilip Baluguri <dilip.baluguri@orbitzworldwide.com>
2015-08-26 15:25:10 -0500

    Adding service layer

--- src/main/java/com/packt/webstore/controller/ProductController.java
+++ src/main/java/com/packt/webstore/controller/ProductController.java
@@ -1,12 +1,9 @@
 package com.packt.webstore.controller;
 
-import java.math.BigDecimal;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import com.packt.webstore.domain.Product;
 import com.packt.webstore.domain.repository.ProductRepository;
 
 @Controller
